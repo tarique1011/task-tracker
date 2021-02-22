ab81231c07e3255398fbb30f57afc9037df3362a
Tarique Javaid <tarique.javaid11@gmail.com>
2021-02-22 19:56:35 +0530

    Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  extends: ['standard', 'standard-react'],
+  rules: {
+    'react/prop-types': 0,
+    camelcase: 'off',
+    'dot-notation': 'off'
+  }
+}
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class SplashScreen extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>SplashScreen</Text>
+      </View>
+    )
+  }
+}
+
+export { SplashScreen }
